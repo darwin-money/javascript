583425ae51aef502deb20aa793e6666affd98c16
Divine508 <onuorahchidera044@gmail.com>
2024-11-18 17:47:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+     return str.split('').reverse().join('');
+ }
+ 
+ // Example
+ console.log(reverseString("hello")); // ➞ "olleh"
+ console.log(reverseString("world")); // ➞ "dlrow"
+ 
